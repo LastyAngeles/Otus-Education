cc821a81157ae1c8094b9de594f8009a07d62dec
Lasty Angeles <maksimmeshkovru@gmail.com>
2018-11-19 11:51:26 +0300

    *jar finally works

--- src/main/java/com/education/Main.java
+++ src/main/java/com/education/Main.java
@@ -1,5 +1,8 @@
+package com.education;
+
 public class Main {
     public static void main(String[] args) {
         Test test = new Test();
+        System.out.println("Print test name: " + test.getName() );
     }
 }
